3dbb3439b07e62d94fa7b462868ae5d68da9b0a8
Andresss21 <andrs27999@gmail.com>
2019-11-17 21:31:49 -0600

    Add files via upload

---
+++ Laboratorios/Laboratorio 6/2.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+
+struct nodo{
+    int info;
+    struct nodo *izq;
+    struct nodo *der;
+};
+typedef struct nodo Nodo;
+typedef struct nodo *Arbol;
+
+Arbol crearArbol(int x){
+    Arbol p = new Nodo;
+    p->info = x;
+    p->izq = NULL;
+    p->der = NULL;
+    return p;
+}
+
+int max(int a, int b) {
+   return ((a > b) ? a : b);
+}
+
+void asignarIzq(Arbol a, int unDato){
+    if(a == NULL)
+        cout << "Error: arbol vacio" << endl;
+    else if(a->izq != NULL)
+        cout << "Error: subarbol IZQ ya existe" << endl;
+    else
+        a->izq = crearArbol(unDato);
+}
+
+void asignarDer(Arbol a, int unDato){
+    if(a == NULL)
+        cout << "Error: arbol vacio" << endl;
+    else if(a->der != NULL)
+        cout << "Error: subarbol DER ya existe" << endl;
+    else
+        a->der = crearArbol(unDato);
+}
+
+void agregarNodo(Arbol a){
+
+    int numero = 0;
+    cout << "Numero a agregar: ";
+    cin >> numero;
+
+    Arbol p = a;
+
+
+    while(true){
+        if(numero == p->info){
+            cout << "Error: " << numero << " ya existe" << endl;
+            return;
+        }
+        else if(numero < p->info){
+            if(p->izq == NULL)
+                break;
+            else
+                p = p->izq;
+        }
+        else{
+            if(p->der == NULL)
+                break;
+            else
+                p = p->der;
+        }
+    }
+
+
+    if(numero < p->info)
+        asignarIzq(p, numero);
+    else
+        asignarDer(p, numero);
